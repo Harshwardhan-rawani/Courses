ac45b745d4977a8ff90c21e4acaec10543ed391a
Harshwardhan <harshwardhan.rawani@gmail.com>
2024-08-04 23:59:20 +0530

    let

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Pages/Home/Home';
 import Login from './Components/Login/Login';
@@ -10,15 +10,15 @@ import Service from './Pages/Service/Service';
 import Contact from './Pages/Contact/Contact';
 import CodeRunner from './Pages/contest/CodeRunner';
 import Enroll from './Components/CoursesIcon/Enroll';
-import VideoUploadForm from './Admin/VideoUploadForm';
+import VideoUploadForm from './Admin/videoupload';
 import AdminHome from './Admin/AdminHome';
 import Sidebar from './Admin/Sidebar';
-import Dashboard from './Admin/Dashboard';
+import Dashbord from './Admin/Dashbord';
 import Courses from './Admin/Courses';
 import Videoplayer from './Components/videoplayer/videoplayer';
 
 function App() {
- 
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -28,24 +28,26 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/about" element={<About />} />
-          <Route path="/course" element={<Course />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/compiler" element={<CodeRunner />} />
-          <Route path="/video/:name" element={<Videoplayer />} />
-          <Route path="/enroll/:name" element={<Enroll />} />
-
-          {/* Admin Routes */}
-          <Route path="/admin" element={<Sidebar />}>
-            <Route index element={<AdminHome />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="courses" element={<Courses />} />
-            <Route path="courses/:name" element={<VideoUploadForm />} />
-          </Route>
+          <Route path="/course" element={<Course/>} />
+          <Route path="/service" element={<Service/>} />
+          <Route path="/contact" element={<Contact/>} />
+          <Route path="/compiler" element={<CodeRunner/>} />
+          <Route path="/video/:name" element={<Videoplayer/>} />
+          
+          <Route path="/admin" element={<><Sidebar/><AdminHome/></>} />
+          <Route path="/enroll/:name" element={<Enroll/>} />
+          {/* admin */}
+          <Route path="/admin/*" element={<Sidebar />}>
+             <Route path="dashboard" element={<Dashbord />} />
+             <Route path="courses" element={<Courses />} />
+             <Route path="courses/:name" element={<VideoUploadForm/>} />
+         
+        </Route>
+          
         </Routes>
       </BrowserRouter>
     </>
-  );
+  )
 }
 
 export default App;
