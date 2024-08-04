85da63c2143695c7fecc2e5e19be4d8002c45fa6
Harshwardhan <harshwardhan.rawani@gmail.com>
2024-08-05 02:00:29 +0530

    test

--- server/index.js
+++ server/index.js
@@ -14,13 +14,13 @@ const compiler = require("./routes/compiler")
 const video = require("./routes/video")
 const authroute = require("./routes/Auth")
 
-const bodyParser = require('body-parser');
+
 Port = process.env.PORT || 3000
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
 app.use(express.static(path.join(__dirname)));
 app.use(cors())
-app.use(bodyParser.json());
+
 
 app.use('/api', compiler);
 app.use("/",authroute)
